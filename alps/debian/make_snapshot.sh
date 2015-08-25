f4879bd725b7f8f87bd51d6dbaa446648157f0ca
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2015-08-25 09:45:55 +0900

    Added simplemc and snap2vtk Added make_snapshot.sh tool

---
+++ alps/debian/make_snapshot.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+REVISION="$1"
+DATE="$2"
+
+URL=https://alps.comp-phys.org/svn/alps1/trunk/alps
+
+if [ -z "$REVISION" ]; then
+    REVISION=$(LANG=C TZ=GMT0 svn info "$URL" | grep '^Last Changed Rev:' | awk '{print $4}')
+    DATE=$(LANG=C TZ=GMT0 svn info "$URL" | grep '^Last Changed Date:' | awk '{print $4}')
+elif [ -z "$DATE" ]; then
+    DATE=$(LANG=C TZ=GMT0 svn info "$URL" | grep '^Last Changed Date:' | awk '{print $4}')
+fi
+DATE=$(echo "$DATE" | sed 's/-//g')
+
+echo "REVISION=$REVISION"
+echo "DATE=$DATE"
+
+DIR="alps-$DATE-r$REVISION"
+rm -rf "$DIR"
+svn export -r "$REVISION" "$URL" "$DIR"
+tar zcvf "$DIR.tar.gz" "$DIR"
