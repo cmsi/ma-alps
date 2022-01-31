aa96a0b96d962afd0201af2fda1b176b70bbed5d
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2022-02-01 08:48:28 +0900

    working

--- build.sh
+++ build.sh
@@ -1,16 +1,1 @@
-#!/bin/sh
-. $(dirname $0)/path.sh
-test -z $BUILD_DIR && exit 127
-
-mkdir -p $TARGET_DIR
-cd $BUILD_DIR 
-case "$(dpkg --print-architecture)" in
-    amd64)
-        dpkg-buildpackage -us -uc
-        mv -f ../${PACKAGE}_${VERSION_BASE}*.changes ../${PACKAGE}_${VERSION_BASE}*.buildinfo ../${PACKAGE}_${VERSION_BASE}*.dsc ../${PACKAGE}_${VERSION_BASE}*.debian.tar.* ../${PACKAGE}_${VERSION_BASE}*.orig.tar.gz ../*${PACKAGE}*${VERSION_BASE}*.deb ../libboost-*${VERSION_BASE}*.deb $TARGET_DIR
-        ;;
-    i386)
-        dpkg-buildpackage -b -us -uc
-        mv -f ../${PACKAGE}_${VERSION_BASE}*.changes ../${PACKAGE}_${VERSION_BASE}*.buildinfo ../*${PACKAGE}*${VERSION_BASE}*.deb ../libboost-*${VERSION_BASE}*.deb $TARGET_DIR
-        ;;
-esac
+../ma-common/script/build.sh
